f066665687827dd83cf12ed25244c4fe8e4edcad
Tamtun <inteslux@yandex.ru>
2025-04-06 19:24:21 +0900

    added generators.py, tests, updated conftest.py, README.md

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from typing import List, Dict
 
 
-def filter_by_state(data: List[Dict[str, str]], state: str = "EXECUTED") -> List[Dict[str, str]]:
+def filter_by_state(data: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """
     Принимает список словарей и опционально значение для ключа.
     Возвращает новый список словарей, содержащий только те словари,
@@ -9,11 +10,10 @@ def filter_by_state(data: List[Dict[str, str]], state: str = "EXECUTED") -> List
     """
     return [item for item in data if item.get("state") == state]
 
-
-def sort_by_date(data: List[Dict[str, str]], descending: bool = True) -> List[Dict[str, str]]:
+def sort_by_date(data: List[Dict], descending: bool = True) -> List[Dict]:
     """
     Принимает список словарей и необязательный параметр,
     задающий порядок сортировки.
-    Возвращает новый список, отсортированный по дате
+    Возвращает новый список, отсортированный по дате.
     """
-    return sorted(data, key=lambda x: x["date"], reverse=descending)
+    return sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=descending)
